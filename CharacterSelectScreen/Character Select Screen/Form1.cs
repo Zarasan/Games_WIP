4c44e57695735415ca4bfdbcb026ef9b72db315d
Aleksandar <43384612+Zarasan@users.noreply.github.com>
2020-06-03 15:13:26 +0200

    CharacherSelectScreen

---
+++ CharacterSelectScreen/Character Select Screen/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Character_Select_Screen
+{
+    public partial class Form1 : Form
+    {
+        int gender;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+
+            if (rb.Checked)
+            {
+                gender = 1;
+                pictureBox1.Image = Properties.Resources.male_lumberjack;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+
+            if (rb.Checked)
+            {
+                gender = 2;
+                pictureBox1.Image = Properties.Resources.female_lumberjack;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = sender as ComboBox;
+
+            switch (cb.SelectedItem)
+            {
+                case "Lumberjack":
+                    if (gender == 1)
+                    {
+                        pictureBox1.Image = Properties.Resources.male_lumberjack;
+                    }
+                    else
+                    {
+                        pictureBox1.Image = Properties.Resources.female_lumberjack;
+                    }
+                break;
+                case "Miner":
+                    if (gender == 1)
+                    {
+                        pictureBox1.Image = Properties.Resources.male_lumberjack;
+                    }
+                    else
+                    {
+                        pictureBox1.Image = Properties.Resources.female_Miner;
+                    }
+                    break;
+                case "Herbalist":
+                    if (gender == 1)
+                    {
+                        pictureBox1.Image = Properties.Resources.male_lumberjack;
+                    }
+                    else
+                    {
+                        pictureBox1.Image = Properties.Resources.female_Herbalist;
+                    }
+                    break;
+            }
+
+        }
+    }
+}
---
+++ CharacterSelectScreen/Character Select Screen/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Character Select Screen")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Character Select Screen")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("11bc6c48-fcfa-4c0f-b059-d480c844e107")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
